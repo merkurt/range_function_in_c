9cbb7f0d08d98f8f4a070beabcac743e80d03748
merkurt <omerkurt099@gmail.com>
2020-11-06 10:35:37 +0300

    init

--- range.h
+++ range.h
@@ -1,7 +1,7 @@
 #ifndef __RANGE_
 #define __RANGE_
 
-void DRANGE(double* arr, double start, double end, int step);
-void FRANGE(float* arr, float start, float end, int step);
+void DRANGE(double* arr, double start, double end, int step); // for double
+void FRANGE(float* arr, float start, float end, int step); // for float
 
 #endif // __RANGE_
